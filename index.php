5c704e96953199fac3e384ac0355888bb59be825
ismailimanov <ismail@imanov.dk>
2017-05-06 00:07:45 +0200

    Materialize CSS er tilføjet til projektet. Vi har valgt dette over Bootstrap eller andre grid systemer, da Materialize CSS fokusere mere på UI/UX, end andre systemer gør.

--- index.php
+++ index.php
@@ -3,9 +3,26 @@
     <head>
         <title>FADBAMSEVALG17 - Kommunalvalg 2017</title>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" type="text/css" rel="stylesheet">
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
     </head>
     <body>
-        Indhold
+        <div class="container">
+            <div class="row">
+                <div class="col s12">Test</div>
+            </div>
+            <div class="row">
+                <div class="col s12 m2">asd</div>
+                <div class="col s12 m2">asd</div>
+                <div class="col s12 m2">asd</div>
+                <div class="col s12 m2">asd</div>
+                <div class="col s12 m2">asd</div>
+                <div class="col s12 m2">asd</div>
+            </div>
+        </div>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
     </body>
 </html>
