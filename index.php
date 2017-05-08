207bfae3fa8f408f0fd54e75471067946cdc5479
ismailimanov <ismail@imanov.dk>
2017-05-08 22:35:11 +0200

    Ændret protokol til HTTPS

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" type="text/css" rel="stylesheet">
-        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
     </head>
     <body>
@@ -46,7 +46,7 @@
                     <p class="flow-text">Kommunalvalg bør interessere dig, hvis du ønsker flere billige ungdomsboliger i din kommune. Federe skaterbaner, sportsmuligheder og flere mødesteder, hvor i kan hænge ud. Bedre og større parkeringspladser til din øse, og ikke mindst flottere cykelstier, så turen bliver meget federe. Hvis du er træt af for lidt busforbindelser og for høje priser, og hvis du mangler et fritidsjob tæt på dit hjem! Hvis du uheldigvis står uden hjem eller har været igennem misbrug eller et liv i kriminalitet, så vil din indflydelse på kommunevalget kunne gøre en forskel på de her ting ud af mange mange flere!</p>
                 </div>
                 <div class="col s12 m4 l4 info">
-                    <img src="http://placehold.it/150x200"><br>
+                    <img src="https://placehold.it/150x200"><br>
                     <span>Fornavn Efternavn</span>
                     <p>Bla bla bla bla</p>
                 </div>
@@ -60,13 +60,13 @@
             </div>
             <div class="row">
                 <div class="col s12 m12 l4 insta">
-                    <img src="http://placehold.it/400x420">
+                    <img src="https://placehold.it/400x420">
                 </div>
                 <div class="col s12 m12 l4 insta">
-                    <img src="http://placehold.it/400x420">
+                    <img src="https://placehold.it/400x420">
                 </div>
                 <div class="col s12 m12 l4 insta">
-                    <img src="http://placehold.it/400x420">
+                    <img src="https://placehold.it/400x420">
                 </div>
             </div>
         </div>
