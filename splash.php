dfa39e1eb4c0803c535845ec75123d8fe17c9bca
Ismail Imanov <ismail@imanov.dk>
2017-05-26 10:28:26 +0200

    Aktivering af loading skærm. Tilføjet sitemap til google analytics

--- splash.php
+++ splash.php
@@ -14,6 +14,17 @@ $sidensLink = str_replace("/TV2-Lorry/loading?side=", "", $sidensLink);
         <link href="css/splashscreen.css" type="text/css" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <meta name="keywords" content="FADBAMSEVALG, FADBAMSEVALG17, FADBAMSEVALG 2017, Kommunalvalg, Kommunalvalg17, Kommunalvalg 2017, Valg, Valg17, Valg 2017, Grøn Koncert, Grøn Koncert 2018, Grøn Koncert Billetter">
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-100004643-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
     </head>
     <body>
     <div class="splashscreen">
